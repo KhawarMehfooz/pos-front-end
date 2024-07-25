e136ebee4c5bece0e0332b9ec6cba866986aa344
Khawar Mehfooz <khawarmehfooz565@gmail.com>
2024-07-25 08:58:39 +0500

    implement logout functionality

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -3,7 +3,20 @@ import Category from '@/components/Category/Category.vue'
 import Inventory from '@/components/Inventory/Inventory.vue'
 import Pos from '@/components/Pos/Pos.vue'
 import Settings from '@/components/Settings/Settings.vue'
+import Cookies from 'js-cookie';
+import { toast } from 'vue3-toastify';
+import { useRouter } from 'vue-router'
+const router = useRouter()
 
+const logOut = async () => {
+    Cookies.remove('auth_token')
+    toast('Logging out....', {
+        type: 'info',
+        autoClose: 2000
+    })
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    router.push('/login');
+}
 
 </script>
 <template>
@@ -14,7 +27,9 @@ import Settings from '@/components/Settings/Settings.vue'
                 <Inventory />
             </div>
             <div class="flex items-center gap-4">
-                <Settings/>
+                <Settings />
+                <button title="Log out" class="text-xl bg-red-600 text-white py-2 px-3 rounded-sm" @click="logOut"><i
+                        class="fa-solid fa-right-from-bracket"></i></button>
             </div>
         </div>
         <Pos />
